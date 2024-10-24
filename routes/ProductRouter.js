b6760226a28fe030d5eac781dc7d729cfa84690f
Guilherme Teixeira <teixeira308@hotmail.com>
2024-10-24 08:24:04 -0300

    fix on getallproducts api and queries

--- routes/ProductRouter.js
+++ routes/ProductRouter.js
@@ -7,10 +7,10 @@ const ProductController = require('../controller/ProductController');
 
 // Rota para receber dados de um candidato
 router.post('/products', login.required, ProductController.createProduct ); 
-router.get('/products', login.required, ProductController.listAllProducts );
+router.get('/products/:user_id', login.required, ProductController.listAllProducts );
 router.get('/products/category/:category_id',login.required, ProductController.simpleListAllProducts );
 router.get('/products/:id', login.required, ProductController.getProduct );
-router.delete('/products/:id', login.required,ProductController.deleteProduct);
+router.delete('/products/:id', login.required,ProductController.deleteProduct); 
 router.put('/products/:id', login.required,ProductController.alterProduct);
-router.put('/products/reorder', login.required,ProductController.reorderProducts);
+
 module.exports = router;
