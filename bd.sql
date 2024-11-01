51efc6e42bf036c3be704d52e15e1df363229bcd
Guilherme Teixeira <teixeira308@hotmail.com>
2024-11-01 11:12:52 -0300

    bd script

--- bd.sql
+++ bd.sql
@@ -1,81 +1,311 @@
-CREATE DATABASE IF NOT EXISTS intellicatalog;
-USE intellicatalog;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 172.17.0.2:3306
+-- Tempo de geração: 01/11/2024 às 12:43
+-- Versão do servidor: 8.4.0
+-- Versão do PHP: 8.2.8
 
-SET GLOBAL time_zone = '-03:00';
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+03:00";
 
 
-DROP TABLE IF EXISTS `intellicatalog`.`products_images`;
-DROP TABLE IF EXISTS `intellicatalog`.`products`;
-DROP TABLE IF EXISTS `intellicatalog`.`categories`;
-DROP TABLE IF EXISTS `intellicatalog`.`stores`;
-DROP TABLE IF EXISTS `intellicatalog`.`users_catalog`;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Banco de dados: `intellicatalog`
+--
 
-CREATE TABLE users_catalog (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(100) NOT NULL,
-    status ENUM('ativo', 'inativo') NOT NULL DEFAULT 'ativo',
-    created_atUTC TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_atUTC TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+-- --------------------------------------------------------
 
-CREATE TABLE stores (
-    id SERIAL PRIMARY KEY,
-    namestore VARCHAR(255) NOT NULL,
-    opening_hours VARCHAR(255),
-    closing_hours VARCHAR(255),
-    status VARCHAR(50) NOT NULL,
-    identificadorexterno VARCHAR(100) NOT NULL,
-    address VARCHAR(255),
-    phone VARCHAR(50),
-    email VARCHAR(100),
-    user_id INT,
-    created_atUTC TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_atUTC TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users_catalog(id) ON DELETE SET NULL
-);
--- Criação da tabela categories (sessões)
-CREATE TABLE categories (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    user_id INT,
-    description TEXT,
-    status ENUM('ativo', 'inativo') NOT NULL DEFAULT 'ativo',
-    created_atUTC TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_atUTC TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users_catalog(id) ON DELETE SET NULL
-);
+--
+-- Estrutura para tabela `categories`
+--
 
--- Criação da tabela products (produtos)
-CREATE TABLE products (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(100) NOT NULL,
-    brand VARCHAR(100) NOT NULL,
-    description TEXT,
-    price DECIMAL(10, 2) NOT NULL,
-    unit VARCHAR(100) NOT NULL,
-    unitquantity VARCHAR(100) NOT NULL,
-	status ENUM('ativo', 'inativo') NOT NULL DEFAULT 'ativo',
-    category_id INT,
-    user_id INT,
-    created_atUTC TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_atUTC TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE SET NULL,
-    FOREIGN KEY (user_id) REFERENCES users_catalog(id) ON DELETE SET NULL
-);
+CREATE TABLE `categories` (
+  `id` int NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `catalog_order` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `description` text,
+  `status` enum('ativo','inativo') NOT NULL DEFAULT 'ativo',
+  `created_atUTC` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_atUTC` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE products_images (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    description TEXT,
-    nomearquivo VARCHAR(100) NOT NULL,
-    status ENUM('ativo', 'inativo') NOT NULL DEFAULT 'ativo',
-    tipo VARCHAR(100) NOT NULL,
-    tamanho VARCHAR(100) NOT NULL,
-    product_id INT,
-     user_id INT,
-    created_atUTC TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_atUTC TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE SET NULL,
-    FOREIGN KEY (user_id) REFERENCES users_catalog(id) ON DELETE SET NULL
-);
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `products`
+--
+
+CREATE TABLE `products` (
+  `id` int NOT NULL,
+  `titulo` varchar(100) NOT NULL,
+  `product_order` int DEFAULT NULL,
+  `brand` varchar(100) NOT NULL,
+  `description` text,
+  `price` decimal(10,2) NOT NULL,
+  `promocional_price` decimal(10,2) DEFAULT NULL,
+  `unit` varchar(100) NOT NULL,
+  `unitquantity` varchar(100) NOT NULL,
+  `status` enum('ativo','inativo') NOT NULL DEFAULT 'ativo',
+  `category_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `created_atUTC` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_atUTC` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `store_id` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `products_images`
+--
+
+CREATE TABLE `products_images` (
+  `id` int NOT NULL,
+  `description` text,
+  `nomearquivo` varchar(100) NOT NULL,
+  `status` enum('ativo','inativo') NOT NULL DEFAULT 'ativo',
+  `tipo` varchar(100) NOT NULL,
+  `tamanho` varchar(100) NOT NULL,
+  `product_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `created_atUTC` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_atUTC` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `stores`
+--
+
+CREATE TABLE `stores` (
+  `id` int NOT NULL,
+  `namestore` varchar(255) NOT NULL,
+  `opening_hours` varchar(255) DEFAULT NULL,
+  `closing_hours` varchar(255) DEFAULT NULL,
+  `status` varchar(50) NOT NULL,
+  `identificadorexterno` varchar(100) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `phone` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `created_atUTC` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_atUTC` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `delivery_fee` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `store_config`
+--
+
+CREATE TABLE `store_config` (
+  `id` int NOT NULL,
+  `store_id` int NOT NULL,
+  `cor_primaria` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cor_secundaria` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cor_botao_primaria` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cor_botao_secundaria` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `taxa_entrega` double NOT NULL,
+  `numero_whatsapp` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `instagram` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `facebook` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `usa_Status` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `chave_pix` varchar(100) NOT NULL,
+  `usa_estoque` varchar(100) NOT NULL,
+  `usa_logo_fundo` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `store_images`
+--
+
+CREATE TABLE `store_images` (
+  `id` int NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `nomearquivo` varchar(100) DEFAULT NULL,
+  `tipo` varchar(45) DEFAULT NULL,
+  `tamanho` varchar(45) DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `store_id` int DEFAULT NULL,
+  `created_atUTC` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_atUTC` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `users_catalog`
+--
+
+CREATE TABLE `users_catalog` (
+  `id` int NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `status` enum('ativo','inativo') NOT NULL DEFAULT 'ativo',
+  `created_atUTC` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_atUTC` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Índices de tabela `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id` (`category_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `fk_store` (`store_id`);
+
+--
+-- Índices de tabela `products_images`
+--
+ALTER TABLE `products_images`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Índices de tabela `stores`
+--
+ALTER TABLE `stores`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Índices de tabela `store_config`
+--
+ALTER TABLE `store_config`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `store_id_f` (`store_id`);
+
+--
+-- Índices de tabela `store_images`
+--
+ALTER TABLE `store_images`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_store_image_store` (`store_id`),
+  ADD KEY `fk_store_image_user` (`user_id`);
+
+--
+-- Índices de tabela `users_catalog`
+--
+ALTER TABLE `users_catalog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `products_images`
+--
+ALTER TABLE `products_images`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `stores`
+--
+ALTER TABLE `stores`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `store_config`
+--
+ALTER TABLE `store_config`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `store_images`
+--
+ALTER TABLE `store_images`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `users_catalog`
+--
+ALTER TABLE `users_catalog`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `categories`
+--
+ALTER TABLE `categories`
+  ADD CONSTRAINT `categories_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users_catalog` (`id`) ON DELETE SET NULL;
+
+--
+-- Restrições para tabelas `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `fk_store` FOREIGN KEY (`store_id`) REFERENCES `stores` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users_catalog` (`id`) ON DELETE SET NULL;
+
+--
+-- Restrições para tabelas `products_images`
+--
+ALTER TABLE `products_images`
+  ADD CONSTRAINT `products_images_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `products_images_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users_catalog` (`id`) ON DELETE SET NULL;
+
+--
+-- Restrições para tabelas `stores`
+--
+ALTER TABLE `stores`
+  ADD CONSTRAINT `stores_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users_catalog` (`id`) ON DELETE SET NULL;
+
+--
+-- Restrições para tabelas `store_config`
+--
+ALTER TABLE `store_config`
+  ADD CONSTRAINT `store_id_f` FOREIGN KEY (`store_id`) REFERENCES `stores` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Restrições para tabelas `store_images`
+--
+ALTER TABLE `store_images`
+  ADD CONSTRAINT `fk_store_image_store` FOREIGN KEY (`store_id`) REFERENCES `stores` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_store_image_user` FOREIGN KEY (`user_id`) REFERENCES `users_catalog` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
