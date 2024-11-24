0b784ace53bf14dc9d18b5ede40ea528d68c94c7
Guilherme Teixeira <teixeira308@hotmail.com>
2024-11-24 10:48:54 -0300

    fix gauth

--- services/GAuth.js
+++ services/GAuth.js
@@ -1,52 +1,29 @@
 const { google } = require('googleapis');
-const path = require('path');
-const fs = require('fs');
+const { getGoogleOAuthClient } = require('../services/dbService'); // Serviço para acessar o banco
 
-// Caminho para o arquivo de credenciais
-const CLIENT_SECRET_PATH = path.join(__dirname, 'credentials.json');
-const SCOPES = ['https://www.googleapis.com/auth/calendar'];
-
-// Carregar credenciais
-let credentials;
-try {
-  credentials = JSON.parse(fs.readFileSync(CLIENT_SECRET_PATH));
-} catch (error) {
-  console.error('Erro ao carregar o arquivo de credenciais:', error.message);
-  throw new Error('Credenciais inválidas ou não encontradas');
-}
-
-const { client_id, client_secret, redirect_uris } = credentials.web;
-
-// Inicializar o cliente OAuth2
-const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
-
-// Gerar URL de autenticação
-function getAuthUrl() {
+async function getAuthUrl(clientId) {
+  const oAuth2Client = await createOAuth2Client(clientId);
   return oAuth2Client.generateAuthUrl({
     access_type: 'offline',
-    scope: SCOPES,
+    scope: ['https://www.googleapis.com/auth/calendar'],
   });
 }
 
-// Retornar cliente OAuth2
-function getOAuth2Client() {
-  return oAuth2Client;
+async function setTokens(clientId, code) {
+  const oAuth2Client = await createOAuth2Client(clientId);
+  const { tokens } = await oAuth2Client.getToken(code);
+  oAuth2Client.setCredentials(tokens);
+  return tokens;
 }
 
-// Obter tokens a partir de um código de autenticação
-async function setTokens(code) {
-  try {
-    const { tokens } = await oAuth2Client.getToken(code);
-    oAuth2Client.setCredentials(tokens);
-    return tokens;
-  } catch (error) {
-    console.error('Erro ao obter tokens:', error.message);
-    throw error;
-  }
+async function createOAuth2Client(clientId) {
+  const credentials = await getGoogleOAuthClient(clientId);
+  const { client_id, client_secret, redirect_uri } = credentials;
+
+  return new google.auth.OAuth2(client_id, client_secret, redirect_uri);
 }
 
 module.exports = {
   getAuthUrl,
-  getOAuth2Client,
   setTokens,
 };
