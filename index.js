16fba8db18fde01454186a943dd86193437422b8
Guilherme Teixeira <teixeira308@hotmail.com>
2025-01-30 17:11:29 -0300

    api add cors liberation

--- index.js
+++ index.js
@@ -30,6 +30,12 @@ app.use('/intellicatalog/v1/', AppointmentsRouter);
 app.use('/intellicatalog/v1/', OrdersRouter);
 
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use(express.urlencoded({
     extended: true
