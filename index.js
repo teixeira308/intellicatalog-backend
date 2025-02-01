73ef85fdbe98eb9ec4bd22f185daaf57fb055ee6
Guilherme Teixeira <teixeira308@hotmail.com>
2025-02-01 03:33:58 -0300

    fix port

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use(express.urlencoded({
 
      
 // Inicia o servidor
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
